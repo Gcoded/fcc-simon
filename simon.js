b0dbb2d05366d68f00050197dc8348ea89b162e8
Godfrey <godfreyjr@gmail.com>
2018-05-20 17:12:49 -0500

    Refactor code to use 1 highlight function

--- simon.js
+++ simon.js
@@ -3,52 +3,35 @@ const greenButton = {
   number: 1,
   color: '#008000',
   litColor: '#00cc00',
-  sound: document.getElementById('sound1'),
-  highlight: function() {
-    $('#button'+this.number).css('background-color', this.litColor);
-    setTimeout(() => {
-      $('#button'+this.number).css('background-color', this.color);
-    }, 1000);
-  }
+  sound: document.getElementById('sound1')
 }
 
 const redButton = {
   number: 2,
   color: '#b30000',
   litColor: '#ff0000',
-  sound: document.getElementById('sound2'),
-  highlight: function() {
-    $('#button'+this.number).css('background-color', this.litColor);
-    setTimeout(() => {
-      $('#button'+this.number).css('background-color', this.color);
-    }, 1000);
-  }
+  sound: document.getElementById('sound2')
 }
 
 const yellowButton = {
   number: 3,
   color: '#cccc00',
   litColor: '#ffff00',
-  sound: document.getElementById('sound3'),
-  highlight: function() {
-    $('#button'+this.number).css('background-color', this.litColor);
-    setTimeout(() => {
-      $('#button'+this.number).css('background-color', this.color);
-    }, 1000);
-  }
+  sound: document.getElementById('sound3')
 }
 
 const blueButton = {
   number: 4,
   color: '#0000cc',
   litColor: '#0000ff',
-  sound: document.getElementById('sound4'),
-  highlight: function() {
-    $('#button'+this.number).css('background-color', this.litColor);
-    setTimeout(() => {
-      $('#button'+this.number).css('background-color', this.color);
-    }, 1000);
-  }
+  sound: document.getElementById('sound4')
+}
+
+function highlight(button) {
+  $('#button'+button.number).css('background-color', button.litColor);
+  setTimeout(() => {
+    $('#button'+button.number).css('background-color', button.color);
+  }, 1000);
 }
 
 
@@ -68,7 +51,7 @@ $('section').mousedown(function(event) {
       button = blueButton;
       break;
     }
-  button.highlight();
+  highlight(button);
   button.sound.play();
 });
 
